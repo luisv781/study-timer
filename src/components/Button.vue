ec2ddd00bf30188f8f49270be321bb8162acc024
Luis <luis.villela021@gmail.com>
2025-08-05 12:47:45 -0500

    design change

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-defineProps<{
-    label: string;
-}>();
+defineProps<{ label: string }>();
 </script>
 
 <template>
@@ -10,10 +8,11 @@ defineProps<{
 
 <style scoped>
 @reference 'tailwindcss';
+@reference '../style.css';
 
 button {
-    @apply bg-amber-300 text-white font-bold text-3xl py-4 px-6 rounded-xl;
-    @apply hover:bg-amber-500;
+    @apply bg-cream text-indigo-900 font-bold text-3xl py-2 px-4 rounded-xl;
+    @apply hover:bg-cream-dark;
     @apply transition duration-300 ease-in-out;
 }
 </style>
