7c6c868d4cb7edc77d0b2354986ea6799d7f9fe4
Luis <luis.villela021@gmail.com>
2025-08-08 13:12:24 -0500

    nice button animation

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,8 +11,21 @@ defineProps<{ label: string }>();
 @reference '../style.css';
 
 button {
-    @apply bg-cream text-indigo-900 font-bold text-3xl py-2 px-4 rounded-xl;
-    @apply hover:bg-cream-dark;
-    @apply transition duration-300 ease-in-out;
+    @apply bg-button text-indigo-900 font-bold text-3xl py-2 px-4 rounded-xl;
+    @apply hover:bg-button-hover;
+    @apply transition ease-in-out;
+    @apply relative;
+}
+
+button:active {
+    @apply translate-y-2;
+}
+
+button::before {
+    @apply absolute h-full w-full top-0 left-0 -z-10 content-[''];
+    @apply bg-button-hover rounded-xl translate-y-2 transition ease-in-out;
+}
+button:active::before {
+    @apply translate-y-0;
 }
 </style>
